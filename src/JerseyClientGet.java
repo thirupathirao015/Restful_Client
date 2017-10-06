667e3201605c735a7b9e805dcf367b37e3f0c4ec
thirupathirao015 <ramisetti015@gmail.com>
2017-10-06 21:33:53 +0530

    Rest client

---
+++ src/JerseyClientGet.java
@@ -0,0 +1,35 @@
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+public class JerseyClientGet {
+
+  public static void main(String[] args) {
+	try {
+
+		Client client = Client.create();
+
+		WebResource webResource = client
+		   .resource("http://localhost:8081/RESTfulExample/rest/xml/customer/999");
+
+		ClientResponse response = webResource.accept("application/xml")
+                   .get(ClientResponse.class);
+
+		if (response.getStatus() != 200) {
+		   throw new RuntimeException("Failed : HTTP error code : "
+			+ response.getStatus());
+		}
+
+		String output = response.getEntity(String.class);
+
+		System.out.println("Output from Server .... \n");
+		System.out.println(output);
+
+	  } catch (Exception e) {
+
+		e.printStackTrace();
+
+	  }
+
+	}
+}
